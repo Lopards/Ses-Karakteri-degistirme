45a3e506cebdec53acafa6f95940cda0f5887bbd
Lopards <101428835+Lopards@users.noreply.github.com>
2023-04-15 18:30:53 +0300

    Update ses_degistir.py

--- ses_degistir.py
+++ ses_degistir.py
@@ -1,20 +1,37 @@
-import matplotlib.pyplot as plt # veri görselleştirme
-import numpy as np #diziler ile işlem yapmak icin
+import matplotlib.pyplot as plt # ->görselleştirme
+from scipy.io import wavfile #dosya
+from scipy.signal import resample, butter, filtfilt #filtreler
 
-from os.path import dirname, join as pjoin
-from scipy.io import wavfile
-from scipy.signal import resample #ses tonu için
-import scipy.io
 
-samplerate, data = wavfile.read("C:/rise_teknoloji/robot.wav") #ses dosyasını okuma 
 
-#göreselleitirme
-print(data[:, 0])
+samplerate, data = wavfile.read("C:/rise_teknoloji/enkhoca.wav") # Ses dosyasını yükleme
+
+# ses sinayline ayar vererek tonu değiştir
+nyquist_freq = 0.6 * samplerate
+cutoff_freq = 600.0 # Hz
+order = 5
+cutoff = cutoff_freq / nyquist_freq
+b, a = butter(order, cutoff, btype="high")
+#sağ sol kulaklık ayarı?
+data[:, 0] = filtfilt(b, a, data[:, 0])
+data[:, 1] = filtfilt(b, a, data[:, 1])
+
+
+wavfile.write("yeni_erkek.wav", samplerate, data) # kaydet
+
+
+""""
+#keyfe göre yap 
+
+# Ses dosyasını görselleştir  
 length = data.shape[0]
-time = np.linspace(0., length, data.shape[0])
+time = np.linspace(0., length/samplerate, length)
 plt.plot(time, data[:, 0], label="Left channel")
 plt.plot(time, data[:, 1], label="Right channel")
 plt.legend()
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.show()
+"""
+
+
