a4e5c1ee1a102440cd6fc3a8a1d39a195b6c2b42
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-04-20 12:42:56 +0300

    Update ses_degistir.py

--- ses_degistir.py
+++ ses_degistir.py
@@ -1,37 +1,35 @@
-import matplotlib.pyplot as plt # ->görselleştirme
-from scipy.io import wavfile #dosya
-from scipy.signal import resample, butter, filtfilt #tonlar icin
+import matplotlib.pyplot as plt #görselleştirme için
+from scipy.io import wavfile 
+from scipy.signal import resample, butter, filtfilt # resample, butter, filtfilt fonksiyonları yüklenir
+import numpy as np 
+
+samplerate, data = wavfile.read("C:/rise_teknoloji/deneme.wav") 
 
 
-
-samplerate, data = wavfile.read("C:/rise_teknoloji/enkhoca.wav") # Ses dosyasını yükleme
 
 # ses sinayline ayar vererek tonu değiştir
-nyquist_freq = 0.6 * samplerate
+
+nyquist_freq = 6.0 * samplerate # Nyquist frekansı hesaplanır
 cutoff_freq = 600.0 # Hz
-order = 5
-cutoff = cutoff_freq / nyquist_freq
-b, a = butter(order, cutoff, btype="high")
-#sağ sol kulaklık ayarı?
-data[:, 0] = filtfilt(b, a, data[:, 0])
-data[:, 1] = filtfilt(b, a, data[:, 1])
+order = 6 # filtrenin mertebesini belirle
+cutoff = cutoff_freq / nyquist_freq # kesme frekansı hesaplanır
+b, a = butter(order, cutoff, btype="high") #filtre tasarla
+
+data = data.reshape((-1, 2)) #data 1 boyutlu ise 2 ye çıkartıyoruz 
+
+data[:, 0] = filtfilt(b, a, data[:, 0]) # sol kanalın verisi filtreden geçir
+data[:, 0] = filtfilt(b, a, data[:, 0]) # sağ kanalın verisi 
+
+wavfile.write("yeni2_ses.wav", samplerate, data) #   yeni bir dosya oluştur
 
 
-wavfile.write("yeni_erkek.wav", samplerate, data) # kaydet
 
-
-""""
-#keyfe göre yap 
-
-# Ses dosyasını görselleştir  
-length = data.shape[0]
-time = np.linspace(0., length/samplerate, length)
-plt.plot(time, data[:, 0], label="Left channel")
-plt.plot(time, data[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
-"""
-
-
+# Ses dosyasını görselleştirme
+length = data.shape[0] # verinin boyutu alınır
+time = np.linspace(0., length/samplerate, length) # zaman dizisi oluşturulur
+plt.plot(time, data[:, 0], label="Left channel") # sol kanalın verisi çizilir
+plt.plot(time, data[:, 1], label="Right channel") # sağ kanalın verisi çizilir
+plt.legend() # etiketleri ekle
+plt.xlabel("Time [s]") # x ekseni 
+plt.ylabel("Amplitude") # y ekseni etiketi
+plt.show() # çizimi göster
