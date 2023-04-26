3b32e9c6ac272a6ce6c3ecaf849ba16d2a1e2425
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-04-26 17:46:22 +0300

    Update ses_degistir.py

--- ses_degistir.py
+++ ses_degistir.py
@@ -1,35 +1,56 @@
-import matplotlib.pyplot as plt #görselleştirme için
-from scipy.io import wavfile 
-from scipy.signal import resample, butter, filtfilt # resample, butter, filtfilt fonksiyonları yüklenir
-import numpy as np 
-
-samplerate, data = wavfile.read("C:/rise_teknoloji/deneme.wav") 
+import scipy.io.wavfile as wav
+import numpy as np
+import scipy.signal
 
 
+class Ses_Degistir:
+    def __init__(self, dosya_yolu):
+     self.dosya_yolu = dosya_yolu  
+     
+    def pitch_shift(self, pitch):
+        if pitch == 1:  # Erkek sesi
+            self.samplerate, self.data = wav.read(dosya_yolu) #dosya yolu verilen ses dosyası oku
+            self.nyquist_freq = 0.2 * self.samplerate #Nyquist frekansı hesapla
+            self.order = 9
+            cutoff_freq = 600.0
 
-# ses sinayline ayar vererek tonu değiştir
+            cutoff = cutoff_freq / self.nyquist_freq #Kesme frekansı nyquist frekansa bölünerek kesme frekansı hesaplan
+            b, a = scipy.signal.butter(self.order, cutoff, btype="high") #butterworth filtresi
+            filtreli_data = scipy.signal.filtfilt(b, a, self.data) # filtreli ses dosyasını elde et
+            return filtreli_data
 
-nyquist_freq = 6.0 * samplerate # Nyquist frekansı hesaplanır
-cutoff_freq = 600.0 # Hz
-order = 6 # filtrenin mertebesini belirle
-cutoff = cutoff_freq / nyquist_freq # kesme frekansı hesaplanır
-b, a = butter(order, cutoff, btype="high") #filtre tasarla
+        elif pitch == 2:  # Kadın sesi
+            self.samplerate, self.data = wav.read(dosya_yolu)
+            self.nyquist_freq = 1.0 * self.samplerate
+            self.order = 5
+            cutoff_freq = 2500.0
 
-data = data.reshape((-1, 2)) #data 1 boyutlu ise 2 ye çıkartıyoruz 
+            cutoff = cutoff_freq / self.nyquist_freq
+            b, a = scipy.signal.butter(self.order, cutoff, btype="low")
+            filtreli_data = scipy.signal.filtfilt(b, a, self.data)
+            return filtreli_data
+        else:
+            print("Hatali giriş! Lütfen 1 veya 2 tuşlarina basın.")
+            return 
 
-data[:, 0] = filtfilt(b, a, data[:, 0]) # sol kanalın verisi filtreden geçir
-data[:, 0] = filtfilt(b, a, data[:, 0]) # sağ kanalın verisi 
+        #cutoff = cutoff_freq / self.nyquist_freq
+        #b, a = scipy.signal.butter(self.order, cutoff, btype="high")
+        #filtreli_data = scipy.signal.filtfilt(b, a, self.data)
 
-wavfile.write("yeni2_ses.wav", samplerate, data) #   yeni bir dosya oluştur
+    def save_wav(self, dosya_yolu, data):
+        wav.write(dosya_yolu, self.samplerate, np.int16(data))
 
 
-
-# Ses dosyasını görselleştirme
-length = data.shape[0] # verinin boyutu alınır
-time = np.linspace(0., length/samplerate, length) # zaman dizisi oluşturulur
-plt.plot(time, data[:, 0], label="Left channel") # sol kanalın verisi çizilir
-plt.plot(time, data[:, 1], label="Right channel") # sağ kanalın verisi çizilir
-plt.legend() # etiketleri ekle
-plt.xlabel("Time [s]") # x ekseni 
-plt.ylabel("Amplitude") # y ekseni etiketi
-plt.show() # çizimi göster
+if __name__ == '__main__':
+    dosya_yolu = 'C:/rise_teknoloji/deneme.wav'
+    ses_degistirici = Ses_Degistir(dosya_yolu)
+    print("Sesi değiştirmek için 1'e basın (erkek sesi), 2'ye basın (kadın sesi):")
+    choice = int(input())
+    filtreli_data = ses_degistirici.pitch_shift(choice)
+    if filtreli_data is not None:
+        yeni_dosya = 'new21_ses.wav'
+        ses_degistirici.save_wav(yeni_dosya, filtreli_data)
+        print("{} dosyasi başariyla {} dosyasina dönüştürüldü. :D".format(dosya_yolu, yeni_dosya))
+        
+    else:
+        print("dosya donusturulurken bir sorun ollustu :(")
