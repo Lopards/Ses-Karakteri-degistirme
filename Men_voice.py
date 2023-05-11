1ab60371903b5b048ad8a6d3ef00184b74090e55
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-05-11 11:26:03 +0300

    Add files via upload
    Çocuk ve Erkek sesleri için .py dosyaları eklendi

---
+++ Men_voice.py
@@ -0,0 +1,26 @@
+import parselmouth
+from parselmouth.praat import call
+from IPython.display import Audio
+
+def men():
+    sound = parselmouth.Sound("kayit.wav")
+
+    Audio(data=sound.values, rate=sound.sampling_frequency)
+
+
+    manipulation = call(sound, "To Manipulation", 0.03, 75, 500)
+
+    type(manipulation)
+
+    pitch_tier = call(manipulation, "Extract pitch tier")
+
+    call(pitch_tier, "Multiply frequencies", sound.xmin, sound.xmax, 0.5)
+
+    call([pitch_tier, manipulation], "Replace pitch tier")
+    sound_octave_up = call(manipulation, "Get resynthesis (overlap-add)")
+
+    Audio(data=sound_octave_up.values, rate=sound_octave_up.sampling_frequency)
+
+    sound_octave_up.save("Erkek_sesi.wav", "WAV")
+    Audio(filename="Erkek_sesi.wav")
+    
