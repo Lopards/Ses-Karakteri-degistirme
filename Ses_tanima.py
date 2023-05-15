4b1a785e563097f8805b6b6e4b63c41063ee5d83
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-05-15 11:34:54 +0300

    Update Ses_tanima.py
    fonksiyon düzeltildi

--- Ses_tanima.py
+++ Ses_tanima.py
@@ -1,45 +1,65 @@
 import tkinter as tk
 import librosa
-import numpy as np
 from tkinter import filedialog as fd
-def Ses():
- def classify_voice():
-    filename = "kayit.wav"  # kaydedilen ses dosyasının adı
-    signal, rate = librosa.load(filename, sr=None, mono=False)
+import numpy as np
 
-    # Ses sinyalini analiz etmek ve sonucu etiketlemek için gereken işlemler burada yapılacak.
+class classify_voice:
+    def __init__(self, result_label):
+        self.result_label = result_label
 
-    # Örnek olarak, sadece kadın ve erkek sınıfları varsa, örnek bir karar aşaması yazalım:
-    if np.mean(signal[0]) > np.mean(signal[1]):
-        gender = "Kadın"
-    else:
-        gender = "Erkek"
+    def classify(self, filename):
+        signal, rate = librosa.load(filename, sr=None, mono=False)
 
-    # Sonucu bir etiketle göster
-    result_label.configure(text=f"Ses {gender} sesine benziyor.")
+        # Ses sinyalini analiz etmek ve sonucu etiketlemek için gereken işlemler burada yapılacak.
 
-    # Tkinter penceresi oluşturma
-    window = tk.Tk()
-    window.title("Ses Sınıflandırma Programı")
+        
+        if np.mean(signal[0]) > np.mean(signal[1]):
+            gender = "Kadın"
+        else:
+            gender = "Erkek"
 
-    # Dosya seçme butonu oluşturma
-    file_label = tk.Label(window, text="Ses dosyası: ")
-    file_label.pack(side=tk.LEFT)
-    file_entry = tk.Entry(window)
-    file_entry.pack(side=tk.LEFT)
-    browse_button = tk.Button(window, text="Dosya Seç", command=fd.askopenfilename(initialdir="/", title="Dosya Seç",
-                                        filetypes=(("Text files", "*.txt*"), ("all files", "*.*"))))
-    browse_button.pack(side=tk.LEFT)
+        # Sonucu bir etiketle göster
+        self.result_label.configure(text=f"Ses {gender} sesine benziyor.")
 
-    # Sınıflandırma butonu oluşturma
-    classify_button = tk.Button(window, text="Sınıflandır", command=classify_voice)
-    classify_button.pack(side=tk.LEFT)
 
-    # Sonuç etiketi oluşturma
-    result_label = tk.Label(window, text="")
-    result_label.pack()
 
-    # Pencereyi çalıştırma
-    window.mainloop()
+class SesSiniflandirmaProgrami:
+    def __init__(self):
+        # Tkinter penceresi oluşturma
+        self.window = tk.Tk()
+        self.window.title("Ses Sınıflandırma Programı")
 
+        # Ses dosyası 
+        file_label = tk.Label(self.window, text="Ses dosyası: ")
+        file_label.pack(side=tk.LEFT)
+        self.file_entry = tk.Entry(self.window)
+        self.file_entry.pack(side=tk.LEFT)
+        browse_button = tk.Button(self.window, text="Dosya Seç", command=self.dosya_ara)
+        browse_button.pack(side=tk.LEFT)
+
+        # Sınıflandırma 
+        classify_button = tk.Button(self.window, text="Sınıflandır", command=self.classify)
+        classify_button.pack(side=tk.LEFT)
+        
+        
+        # Sonuç etiketi
+        self.result_label = tk.Label(self.window, text="")
+        self.result_label.pack()
+        self.voice_classifier = classify_voice(self.result_label) 
+        
+        self.filename = ""  # Dosya adı için boş bir özellik oluşturun
+        
+        # Pencereyi çalıştırma
+        self.window.mainloop()
+   
+    def dosya_ara(self):
+        self.filename = fd.askopenfilename()  # Dosya adını self.filename'e ata
+        self.file_entry.delete(0, tk.END)
+        self.file_entry.insert(tk.END, self.filename)
+ 
+    def classify(self):
+        if self.filename:
+            self.voice_classifier.classify(self.filename)  # Dosya adını sınıflandırma sınıfına geçirin
+        else:
+            self.result_label.configure(text="Lütfen ses dosyası seçin.")
 
