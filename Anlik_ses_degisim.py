0db53a5cc5128802e936fe67479cacac0f5dc5b5
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-05-30 19:22:33 +0300

    Update Anlik_ses_degisim.py
    bas-konuş eklendi

--- Anlik_ses_degisim.py
+++ Anlik_ses_degisim.py
@@ -9,10 +9,12 @@ import soundfile as sf
 class SesDegisim:
     def __init__(self):
         self.root = tk.Tk()
-        self.basla_button = tk.Button(self.root, text="Başla", command=self.ses_degisimi_baslat)
+        self.basla_button = tk.Button(self.root, text="Başlat", command=self.ses_degisimi_baslat)
         self.basla_button.pack()
         self.durdur_button = tk.Button(self.root, text="Durdur", command=self.ses_degisimi_durdur)
         self.durdur_button.pack()
+        self.bas_konus_button = tk.Button(self.root, text="BAS-KONUS", command=self.bas_konus)
+        self.bas_konus_button.pack()
 
         self.CHUNK = 1024
         self.FORMAT = pyaudio.paFloat32
@@ -23,7 +25,23 @@ class SesDegisim:
         self.p = pyaudio.PyAudio()
         self.stream = None
         self.is_running = False
+        self.is_paused = False
         self.thread = None
+
+        self.root.bind('<Key>', self.klavye_kontrol)
+
+    def klavye_kontrol(self, event):
+        if event.char == 'q': # q ile duraklat-devam et 
+            if self.is_running and not self.is_paused:
+                self.is_paused = True
+            elif self.is_running and self.is_paused:
+                self.is_paused = False
+                self.bas_konus()
+
+    def bas_konus(self):
+        if self.thread is None or not self.thread.is_alive():
+            self.thread = threading.Thread(target=self.baslat_ses_degisimi)
+            self.thread.start()
 
     def ses_degisimi_baslat(self):
         if self.thread is None or not self.thread.is_alive():
@@ -48,7 +66,7 @@ class SesDegisim:
                                   frames_per_buffer=self.CHUNK)
 
         kaydedilen_list = []
-        for a in range(int(self.RATE / self.CHUNK * 5)):
+        for a in range(int(self.RATE / self.CHUNK * 2)):
             input_data = self.stream.read(self.CHUNK)
             kaydedilen_list.append(input_data)
 
@@ -60,6 +78,9 @@ class SesDegisim:
             while self.is_running:
                 input_data = self.stream.read(self.CHUNK)
                 audio_data = np.frombuffer(input_data, dtype=np.float32) * 0.4
+
+                if self.is_paused:
+                    continue
 
                 shifted_audio_data = signal.resample(audio_data, int(len(audio_data) * self.PITCH_SHIFT_FACTOR))
 
@@ -82,16 +103,11 @@ class SesDegisim:
     def hata_mesaji(self):
         error_label = tk.Label(self.root, text="HATA: Zaten Erkek sesi!")
         error_label.pack()
-    
 
     def run(self):
-      
         self.root.mainloop()
 
 
 if __name__ == "__main__":
     ses_degisim = SesDegisim()
     ses_degisim.run()
-    t = threading.Thread(target=SesDegisim.hata_mesaji.__code__)
-    t.setDaemon(True)
-    t.start()    
