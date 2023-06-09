15f24a7185ac8520d62e107ed7c53b2985d79001
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-06-10 00:04:06 +0300

    Add files via upload
    intercom ses aktarma

---
+++ intercom/client.py
@@ -0,0 +1,45 @@
+import socket
+import pyaudio
+
+# PyAudio ayarları
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# Bağlantı ayarları
+HOST = 'IP_ADRESS'
+PORT = 12345
+
+# Soket bağlantısı ve ses alma
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+print("* Sunucuya bağlanıldı.")
+
+# Ses oynatma
+def play_sound(stream):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    output=True)
+
+    while True:
+        data = stream.recv(CHUNK)
+        if not data:
+            break
+        stream.write(data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# Soket bağlantısı için PyAudio stream oluşturma
+stream = client_socket.makefile('rb')
+
+print("* Ses alındı ve oynatılıyor...")
+play_sound(stream)
+
+stream.close()
+client_socket.close()
---
+++ intercom/server_woman.py
@@ -0,0 +1,79 @@
+import socket
+import pyaudio
+import numpy as np
+from scipy import signal
+# instant male voice change
+
+# PyAudio ayarları
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# Bağlantı ve soket ayarları
+HOST = 'YOUR_IP_ADRESS'
+PORT = 12345
+
+# Pitch değiştirme faktörü
+PITCH_SHIFT_FACTOR =  0.85
+
+
+# Soket oluşturma ve bağlantıyı bekletme
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
+
+print(f"* Bağlantı için {HOST}:{PORT} dinleniyor...")
+
+client_socket, address = server_socket.accept()
+print(f"* {address} adresinden bir bağlantı alındı.")
+
+# Ses dönüşümü ve gönderme
+def convert_and_send(stream, speaker_stream):
+    while True:
+        data = stream.read(CHUNK)
+        
+        audio_data = np.frombuffer(data, dtype=np.int16)
+     
+        converted_data = signal.resample(audio_data, int(len(audio_data) * PITCH_SHIFT_FACTOR))*1.4
+        converted_data = converted_data.astype(np.int16)
+        converted_data_bytes = converted_data.tobytes()
+        client_socket.sendall(converted_data_bytes)
+       # speaker_stream.write(data)  # Dönüştürülmemiş sesi hoparlörden çal
+            
+"""""
+def determine_gender(data):
+    if np.mean(data) > 0:
+        gender = "woman"
+    else:
+        gender = "man"
+    
+    return gender
+"""""
+
+
+# PyAudio stream oluşturma mic hoparlör
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+# Hoparlör için PyAudio stream oluşturma
+speaker_stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        output=True)
+
+print("* Ses kaydediliyor ve dönüştürülerek gönderiliyor...")
+convert_and_send(stream, speaker_stream)
+
+stream.stop_stream()
+stream.close()
+speaker_stream.stop_stream()
+speaker_stream.close()
+p.terminate()
+
+client_socket.close()
+server_socket.close()
