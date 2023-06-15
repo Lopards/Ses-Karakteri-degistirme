6b75500f63b0152de7a4b8c9f5484b34aa820b09
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-06-16 01:59:08 +0300

    Update client.py
    sunucuya ses gönderebileceğiz. 
    (ekleme ve güncellemeler gelecek.)

--- intercom/client.py
+++ intercom/client.py
@@ -1,45 +1,120 @@
 import socket
 import pyaudio
+import numpy as np
+import tkinter as tk
+import threading
 
-# PyAudio ayarları
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 22050
 
-# Bağlantı ayarları
-HOST = 'IP_ADRESS'
-PORT = 12345
+class Client:
+    def _init_(self):
+        self.CHUNK = 1024
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 22050
 
-# Soket bağlantısı ve ses alma
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+        self.HOST = 'server_ip'  # Sunucu IP adresi
+        self.PORT = 12345  # Sunucu port numarası
 
-print("* Sunucuya bağlanıldı.")
+        self.is_running = False
+        self.server_socket = None
+        self.stream = None
+        self.root = None
 
-# Ses oynatma
-def play_sound(stream):
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True)
+        self.create_interface()
 
-    while True:
-        data = stream.recv(CHUNK)
-        if not data:
-            break
-        stream.write(data)
+    def create_interface(self):
+        self.root = tk.Tk()
+        self.root.title("Ses İletişim Arayüzü")
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+        self.start_button = tk.Button(
+            self.root, text="Başlat", command=self.start_communication)
+        self.start_button.pack()
 
-# Soket bağlantısı için PyAudio stream oluşturma
-stream = client_socket.makefile('rb')
+        self.stop_button = tk.Button(
+            self.root, text="Durdur", command=self.stop_communication)
+        self.stop_button.pack()
 
-print("* Ses alındı ve oynatılıyor...")
-play_sound(stream)
+        self.sesal = tk.Button(self.root, text="ses-al", command=self.ses_al)
+        self.sesal.pack()
 
-stream.close()
-client_socket.close()
+        self.sesal_stop = tk.Button(
+            self.root, text="ses-al-stop", command=self.ses_al_stop)
+        self.sesal_stop.pack()
+
+        self.root.bind("<KeyPress>", self.keyboard_control)
+        self.root.bind("<KeyRelease>", self.keyboard_control)
+
+    def start_communication(self):
+        self.is_running = True
+        threading.Thread(target=self.send_audio).start()
+
+    def ses_al(self):
+        self.is_running = True
+        threading.Thread(target=self.receive_audio,
+                         args=self.is_running).start()
+
+    def ses_al_stop(self):
+        self.is_running = False
+
+    def stop_communication(self):
+        self.is_running = False
+
+    def keyboard_control(self, event):
+        if event.char == 'q':
+            if event.type == tk.EventType.KeyPress:
+                self.send_audio()
+            elif event.type == tk.EventType.KeyRelease:
+                self.stop_communication()
+
+    def send_audio(self):
+        p = pyaudio.PyAudio()
+        self.stream = p.open(format=self.FORMAT,
+                             channels=self.CHANNELS,
+                             rate=self.RATE,
+                             input=True,
+                             frames_per_buffer=self.CHUNK)
+
+        while self.is_running:
+            data = self.stream.read(self.CHUNK)
+            audio_data = np.frombuffer(data, np.int16)
+            self.server_socket.sendall(audio_data)
+
+            if not self.is_running:
+                break
+
+        self.stream.stop_stream()
+        self.stream.close()
+        p.terminate()
+
+    def receive_audio(self, is_running):
+        p = pyaudio.PyAudio()
+        speaker_stream = p.open(format=self.FORMAT,
+                                channels=self.CHANNELS,
+                                rate=self.RATE,
+                                output=True)
+
+        while True:
+            if is_running:
+                data = self.server_socket.recv(self.CHUNK)
+                if not data:
+                    break
+                speaker_stream.write(data)
+
+            else:
+                break
+
+        speaker_stream.stop_stream()
+        speaker_stream.close()
+        p.terminate()
+
+    def run(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.connect((self.HOST, self.PORT))
+
+        self.root.mainloop()
+        self.server_socket.close()
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.run()
