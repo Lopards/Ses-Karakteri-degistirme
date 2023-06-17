486c7b971a9e64e678d82fb17b8b804403d26959
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-06-17 17:21:20 +0300

    Update client.py
    thread ile daha stabil hale getirildi.

--- intercom/client.py
+++ intercom/client.py
@@ -4,18 +4,21 @@ import numpy as np
 import tkinter as tk
 import threading
 
-
 class Client:
-    def _init_(self):
+    def __init__ (self):
         self.CHUNK = 1024
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
         self.RATE = 22050
 
-        self.HOST = 'server_ip'  # Sunucu IP adresi
+        self.HOST = '192.168.1.44'  # Sunucu IP adresi
         self.PORT = 12345  # Sunucu port numarası
 
         self.is_running = False
+        self.is_running_recv = False
+
+        self.event = threading.Event()
+
         self.server_socket = None
         self.stream = None
         self.root = None
@@ -26,20 +29,24 @@ class Client:
         self.root = tk.Tk()
         self.root.title("Ses İletişim Arayüzü")
 
-        self.start_button = tk.Button(
-            self.root, text="Başlat", command=self.start_communication)
+        self.start_button = tk.Button(self.root, text="Başlat", command=self.start_communication)
         self.start_button.pack()
 
-        self.stop_button = tk.Button(
-            self.root, text="Durdur", command=self.stop_communication)
+        self.stop_button = tk.Button(self.root, text="Durdur", command=self.stop_communication)
         self.stop_button.pack()
 
-        self.sesal = tk.Button(self.root, text="ses-al", command=self.ses_al)
-        self.sesal.pack()
+        self.get_sound_button = tk.Button(self.root, text="ses-al", command=self.get_sound)
+        self.get_sound_button.pack()
 
-        self.sesal_stop = tk.Button(
-            self.root, text="ses-al-stop", command=self.ses_al_stop)
-        self.sesal_stop.pack()
+        self.get_sound_contunie_button = tk.Button(self.root, text="ses-al_devam", command=self.get_sound_contunie)
+        self.get_sound_contunie_button.pack()
+
+        self.get_sound_stop_button = tk.Button(self.root, text="ses-al-stop", command=self.get_sound_stop)
+        self.get_sound_stop_button.pack()
+
+
+
+
 
         self.root.bind("<KeyPress>", self.keyboard_control)
         self.root.bind("<KeyRelease>", self.keyboard_control)
@@ -48,13 +55,30 @@ class Client:
         self.is_running = True
         threading.Thread(target=self.send_audio).start()
 
-    def ses_al(self):
-        self.is_running = True
-        threading.Thread(target=self.receive_audio,
-                         args=self.is_running).start()
+        #threading.Thread(target=self.receive_audio).start()
 
-    def ses_al_stop(self):
-        self.is_running = False
+    def get_sound(self):
+        #self.is_running = True
+        #threading.Thread(target=self.send_audio).start()
+        self.is_running_recv = True
+        threading.Thread(target=self.receive_audio).start()
+        self.get_sound_button.config(state="disabled")
+
+    """def baslat_ses_al(self):
+        threading.Thread(target=self.receive_audio).start      
+        self.get_sound_button.config(state="disabled")"""
+    
+    def get_sound_stop(self):
+        self.event.clear()
+
+        self.get_sound_stop_button.config(state="disabled")
+        self.get_sound_contunie_button.config(state="active")
+
+
+    def get_sound_contunie(self):
+        self.event.set()
+        self.get_sound_contunie_button.config(state="disabled")
+        self.get_sound_stop_button.config(state="active")
 
     def stop_communication(self):
         self.is_running = False
@@ -74,44 +98,50 @@ class Client:
                              input=True,
                              frames_per_buffer=self.CHUNK)
 
+        #self.server_socket.sendall(b"START")  # İletişimi başlatmak için sunucuya "START" mesajı gönderilir
+
         while self.is_running:
             data = self.stream.read(self.CHUNK)
-            audio_data = np.frombuffer(data, np.int16)
+            audio_data = np.frombuffer(data,np.int16)
             self.server_socket.sendall(audio_data)
 
             if not self.is_running:
                 break
 
+        #self.server_socket.sendall(b"STOP")  # İletişimi durdurmak için sunucuya "STOP" mesajı gönderilir
+
         self.stream.stop_stream()
         self.stream.close()
         p.terminate()
 
-    def receive_audio(self, is_running):
+    def receive_audio(self):
         p = pyaudio.PyAudio()
-        speaker_stream = p.open(format=self.FORMAT,
+        self.stream = p.open(format=self.FORMAT,
                                 channels=self.CHANNELS,
                                 rate=self.RATE,
                                 output=True)
 
-        while True:
-            if is_running:
-                data = self.server_socket.recv(self.CHUNK)
-                if not data:
-                    break
-                speaker_stream.write(data)
+        while self.is_running_recv:
+            data = self.server_socket.recv(self.CHUNK)
 
-            else:
+            if not data:
                 break
+            if self.event.is_set():
+                self.stream.write(data)
 
-        speaker_stream.stop_stream()
-        speaker_stream.close()
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.server_socket.close()
         p.terminate()
 
     def run(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.connect((self.HOST, self.PORT))
+        print(f"bağlanıldı: {self.HOST}")
 
         self.root.mainloop()
+
         self.server_socket.close()
 
 
