ccd7716c7a616b8d549b7eacfb6d1a64856e3afb
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-07-09 11:24:47 +0300

    Update server_man.py
    *Kullanıcı server ip numarasını arayüzde girebilecek.
    
    *Ses gönderme-duraklatma butonu (Başlat-Durdur) butonları Algoritması yenilendi.

--- intercom/server_man.py
+++ intercom/server_man.py
@@ -3,12 +3,13 @@ import pyaudio
 import numpy as np
 import threading
 import tkinter as tk
+import tkinter.ttk as ttk
 from scipy import signal 
 
 
 class SesIletisimArayuzuE:
     def __init__(self):
-        self.HOST = 'SERVER_IP_ADRESS' 
+        self.HOST = None
         self.PORT = 12345
         self.CHUNK = 1024
         self.CHANNELS = 1
@@ -16,12 +17,14 @@ class SesIletisimArayuzuE:
         self.PITCH_SHIFT_FACTOR = 1.2
 
         self.event = threading.Event()
-
+        
         self.server_socket = None
         self.client_socket = None
         self.stream = None
+        self.output_stream = None
         self.speaker_stream = None
         self.root = None
+        self.ip_file = "ip_addresses.txt"
 
         self.is_running = False
         self.is_running_recv = True
@@ -32,25 +35,36 @@ class SesIletisimArayuzuE:
         self.root.title("Ses İletişim Arayüzü")
         self.root.geometry("250x230")
 
+        self.ip_label = tk.Label(self.root, text="IP Adresi:")
+        self.ip_label.pack()
+
+        self.ip_combobox = ttk.Combobox(self.root, values=self.get_saved_ip_addresses())
+        self.ip_combobox.pack()
+
+        self.connect_button = tk.Button(self.root, text="Bağlan", command=self.connect_to_server)
+        self.connect_button.pack()
+
         self.start_button = tk.Button(
-            self.root, text="Başlat", command=self.start)
-        self.start_button.pack()
+            self.root, text="Başlat",cursor="hand2", command=self.start)
+        self.start_button.pack(pady=3)
 
         self.stop_button = tk.Button(
-            self.root, text="stop", command=self.stop)
+            self.root, text="stop",cursor="hand2", command=self.stop)
         self.stop_button.pack()
 
         self.get_sound_button = tk.Button(
-            self.root, text="Ses Al", command=self.start_get_sound)
-        self.get_sound_button.pack()
+            self.root, text="Ses Al",cursor="hand2", command=self.start_get_sound,disabledforeground="#a9a9a9")
+        self.get_sound_button.pack(pady=3)
+        
+        self.get_sound_contunie_button = tk.Button(
+        self.root, text="Ses Alı Devam Et",cursor="hand2", command=self.get_sound_contunie)
+        self.get_sound_contunie_button.pack()  
         
         self.get_sound_stop_button = tk.Button(
-        self.root, text="Ses Alı Duraklat", command=self.get_sound_stop)
-        self.get_sound_stop_button.pack()
+        self.root, text="Ses Alı Duraklat",cursor="hand2", command=self.get_sound_stop)
+        self.get_sound_stop_button.pack(pady=3)
 
-        self.get_sound_contunie_button = tk.Button(
-        self.root, text="Ses Alı Devam Et", command=self.get_sound_contunie)
-        self.get_sound_contunie_button.pack()       
+             
         
         
         self.root.bind("<KeyPress>", self.klavye_kontrol)
@@ -66,7 +80,8 @@ class SesIletisimArayuzuE:
 
         self.root = tk.Tk()
         self.root.title("Mikrofon Çıkışı Değiştirici")
-        self.root.geometry("280x300")
+        self.root.geometry("280x300+450+100")
+        
 
         self.output_device_list = []
         for i in range(p.get_device_count()):
@@ -87,13 +102,72 @@ class SesIletisimArayuzuE:
         for device in self.output_device_list:
             self.output_device_listbox.insert(tk.END, device[1])
 
-        self.select_button = tk.Button(self.root, text="Seç", command=self.play_button_clicked)
+        self.select_button = tk.Button(self.root, text="Seç", command=self.play_button_clicked,cursor="hand2",activeforeground="red")
         self.select_button.pack()
 
-        self.stop_button = tk.Button(self.root, text="Durdur", command=self.stop_button_clicked)
-        self.stop_button.pack()
+        """self.stop_button = tk.Button(self.root, text="Durdur",cursor="hand2",command=self.stop_button_clicked)
+        self.stop_button.pack()"""
+
+        self.root.mainloop()
         
                             ##### ********** ######
+
+    def connect_to_server(self):
+        selected_ip = self.ip_combobox.get()
+        self.HOST = selected_ip
+
+        if selected_ip not in self.get_saved_ip_addresses():
+            self.save_ip_address(selected_ip)
+        
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.HOST, self.PORT))
+        self.server_socket.listen(1)
+        print(f"* Bağlantı için {self.HOST}:{self.PORT} dinleniyor...")
+
+        self.client_socket, address = self.server_socket.accept()
+        print(f"* {address} adresinden bir bağlantı alındı.")
+        
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16,
+                            channels=self.CHANNELS,
+                            rate=self.RATE,
+                            input=True,
+                            frames_per_buffer=self.CHUNK)
+
+        speaker_stream = p.open(format=pyaudio.paInt16,
+                                    channels=self.CHANNELS,
+                                    rate=self.RATE,
+                                    output=True)
+        self.root.mainloop()
+
+        stream.stop_stream()
+        stream.close()
+        speaker_stream.stop_stream()
+        speaker_stream.close()
+        p.terminate()
+
+        self.client_socket.close()
+        self.server_socket.close()
+
+        
+
+        
+    
+    def get_saved_ip_addresses(self):
+        ip_addresses = []
+        try:
+            with open(self.ip_file, "r") as f:
+                for line in f:
+                    ip_addresses.append(line.strip())
+        except FileNotFoundError:
+            pass
+        return ip_addresses
+
+    def save_ip_address(self, ip_address):
+        with open(self.ip_file, "a") as f:
+            f.write(ip_address + "\n")
+    
+
     def send_audio(self):
         p = pyaudio.PyAudio()
         stream = p.open(format=pyaudio.paInt16,
@@ -143,6 +217,7 @@ class SesIletisimArayuzuE:
         stream.stop_stream()
         stream.close()
         p.terminate()
+
                             ##### ********** ######            
     """ def determine_gender(self,audio_data ):
         if np.mean(audio_data) > 0:
@@ -186,31 +261,34 @@ class SesIletisimArayuzuE:
         stream.close()
         self.client_socket.close()
         p.terminate()
+
                             ##### ********** ######        
     def start_get_sound(self):
         threading.Thread(target=self.get_sound_fonc).start()
-        self.get_sound_button.config(state="disabled" )
+        self.get_sound_button.config(state="normal",cursor="arrow")
 
                             ##### ********** ######
 
     def get_sound_stop(self):
         self.event.clear()
         #self.is_running_recv = False
-        self.get_sound_stop_button.config(state="disabled")
-        self.get_sound_contunie_button.config(state="active")
+        self.get_sound_stop_button.config(state="disabled",cursor="arrow")
+        self.get_sound_contunie_button.config(state="active",cursor="hand2")
                             ##### ********** ######
     def get_sound_contunie(self):
         self.event.set()
-        self.get_sound_contunie_button.config(state="disabled")
-        self.get_sound_stop_button.config(state="active")
+        self.get_sound_contunie_button.config(state="disabled",cursor="arrow")
+        self.get_sound_stop_button.config(state="active",cursor="hand2")
         
                             ##### ********** ######
     def start(self):
-        self.is_running = True
-        threading.Thread(target=self.send_audio).start()
+        if not self.is_running:
+            self.is_running = True
+            threading.Thread(target=self.send_audio).start()
                             ##### ********** ######
     def stop(self):
-        self.is_running = False
+        if self.is_running:
+            self.is_running = False
 
                             ##### ********** ######
     def klavye_kontrol(self, event):
@@ -233,8 +311,12 @@ class SesIletisimArayuzuE:
         )
                             ##### ********** ######
     def play_server_output(self, data):
-        self.output_stream.write(data)
-                            ##### ********** ######
+        if self.output_stream is not None:
+            self.output_stream.write(data)
+        else:
+            print("hoparlör seçiniz...")
+            
+                        ##### ********** ######
     def select_output_device(self):
         device_index = self.output_device_listbox.curselection()
         output_device = self.output_device_list[device_index[0]][0]
@@ -246,45 +328,14 @@ class SesIletisimArayuzuE:
         self.set_output_stream(output_device)
         #self.play_server_output(data)
                             ##### ********** ######
-    def stop_button_clicked(self):
-        self.stop_event.set()
-        
+    """def stop_button_clicked(self):
+        self.stop_event.set()"""
+
+    
                              ##### ********** ######   
-    def run_server(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.HOST, self.PORT))
-        self.server_socket.listen(1)
-
-        print(f"* Bağlantı için {self.HOST}:{self.PORT} dinleniyor...")
-
-        self.client_socket, address = self.server_socket.accept()
-        print(f"* {address} adresinden bir bağlantı alındı.")
-
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16,
-                            channels=self.CHANNELS,
-                            rate=self.RATE,
-                            input=True,
-                            frames_per_buffer=self.CHUNK)
-
-        speaker_stream = p.open(format=pyaudio.paInt16,
-                                    channels=self.CHANNELS,
-                                    rate=self.RATE,
-                                    output=True)
-
-        self.root.mainloop()
-
-        stream.stop_stream()
-        stream.close()
-        speaker_stream.stop_stream()
-        speaker_stream.close()
-        p.terminate()
-
-        self.client_socket.close()
-        self.server_socket.close()
 
 
 if __name__ == "__main__":
     ses_arayuzu = SesIletisimArayuzuE()
-    ses_arayuzu.run_server()
+    
     
